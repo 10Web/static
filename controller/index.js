db1305dff3613c90a9cd6105dbb3bff04445b7d7
gorgear <mac@macdeMacBook-Air.local>
2018-05-18 23:46:35 +0800

    add homepage

--- controller/index.js
+++ controller/index.js
@@ -3,49 +3,41 @@
 var config = require('../config');
 
 /**
- * @api /api/hello_text
- */
-exports.helloText = function (req, callback) {
-  let data = 'hello';
-  callback(null, data, 'text');
-};
-
-/**
  * @api /
+ * @description 用于社团首页的内容
  */
-exports.welcomeTpl = function (req, callback) {
+exports.homepage = function (req, callback) {
   callback(null, {
-    tpl: 'welcome.html',
+    tpl: 'homepage.html',
     data: {
       prefix: config.prefix === '/' ? '' : config.prefix
     }
   }, 'html');
 };
 
-
 /**
- * 如需要使用原始的API, 请加上  nowrap 标记
- * @api /api/hello_origin
- * @nowrap
+ * @api /about
+ * @description 用于社团介绍的内容
  */
-exports.helloOrigin = function (req, res) {
-  res.end('hello');
-};
-
-
-/**
- * 支持 generatorFunction, 使用方式和上面一样，如果需要nowrap, 同样加个注解
- * @api /api/hello_gen
- */
-exports.helloGen = function* (req) {
-  return 'hello';
+exports.about = function (req, callback) {
+  callback(null, {
+    tpl: 'about.html',
+    data: {
+      prefix: config.prefix === '/' ? '' : config.prefix
+    }
+  }, 'html');
 };
 
 /**
- * 支持 asyncFunction, 使用方式和上面一样，如果需要nowrap, 同样加个注解
- * @api /api/hello_async
+ * @api /opensource
+ * @description 用于介绍社团的开源项目
  */
-exports.helloGen = async function (req) {
-  return 'hello';
+exports.opensource = function (req, callback) {
+  callback(null, {
+    tpl: 'opensource.html',
+    data: {
+      prefix: config.prefix === '/' ? '' : config.prefix
+    }
+  }, 'html');
 };
 
